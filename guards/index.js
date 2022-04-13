170a2a04a360c3b818dead65938cdae493a824d9
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-04-13 11:34:07 +0300

    fix bag guards for admin

--- guards/index.js
+++ guards/index.js
@@ -9,7 +9,7 @@ export default {
     },
     checkLinks(data, role) {
         if(role in this.roles) {
-            if(this.roles[role] === 'admin') return data
+            if(role === 'admin') return data
             else return data.filter(item => this.roles[role].includes(item.type))
         } else {
             alert('error role')
@@ -18,7 +18,7 @@ export default {
     },
     checkRouts(type, role) {
         if(role in this.roles) {
-            if(this.roles[role] === 'admin') return true
+            if(role === 'admin') return true
             else return this.roles[role].includes(type)
         } else {
             return false
